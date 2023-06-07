034142b5a5673c4a90cff55ec0af57ad6ff055e1
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-06-07 09:19:45 +0300

    DonE

--- spec/migrations/002_create_shows_spec.rb
+++ spec/migrations/002_create_shows_spec.rb
@@ -1,29 +1,10 @@
-require_relative '../../db/migrate/002_create_shows'
-
-describe CreateShows do
-  before(:all) { DatabaseCleaner.strategy = :truncation }
-  after(:all)  { DatabaseCleaner.strategy = :transaction }
-
-  let(:previous_version) { 1 }
-  let(:current_version) { 2 }
-
-  around do |example|
-    migrate!(:down, previous_version)
-    Show.reset_column_information
-    example.run
-    Show.reset_column_information
-  end
-
-  describe "#change" do
-    it "creates a shows table with a name, day, season and genre column" do
-      migrate!(:up, current_version)
-      expect(Show.columns_hash).to include({
-        "name" => have_attributes(type: :string),
-        "day" => have_attributes(type: :string),
-        "season" => have_attributes(type: :string),
-        "genre" => have_attributes(type: :string)
-      })
+class CreateShows < ActiveRecord::Migration[6.1]
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :day
+      t.string :season
+      t.string :genre
     end
   end
-
 end
